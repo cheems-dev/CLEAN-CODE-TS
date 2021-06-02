4e11a8d0d17cedfb3daf4367187224308053c648
Davaria <Daprimovaria@gmail.com>
2021-06-02 15:06:04 -0500

    chore: resolve error in jest.ts

--- jest.config.ts
+++ jest.config.ts
@@ -5,7 +5,7 @@
 
 export default {
   roots: ['<rootDir>/src'],
-  collectCoverage: ['<rootDir>/src/***/*.ts'],
+  collectCoverage: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
   testEnvironment: 'node',

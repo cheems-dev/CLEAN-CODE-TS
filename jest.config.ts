b1441648d6c404e2f9af965a9c7e4083d2785030
Davaria <Daprimovaria@gmail.com>
2021-07-07 00:13:04 -0500

    chore: config files

--- jest.config.ts
+++ jest.config.ts
@@ -1,10 +1,5 @@
-/*
- * For a detailed explanation regarding each configuration property and type check, visit:
- * https://jestjs.io/docs/configuration
- */
-
 export default {
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/src/**/*'],
   collectCoverage: true,
   coverageDirectory: 'coverage',
   coverageProvider: 'v8',
